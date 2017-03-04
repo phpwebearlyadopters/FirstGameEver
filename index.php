8662147547394bd972b52dce27c3cdab218c8b9d
Mateusz Sienko <msienio@poczta.onet.pl>
2017-03-04 12:40:48 +0100

    Powrót do starej wersji z polaczonym HTML/PHP w jednym pliku,bez template narazie

--- index.php
+++ index.php
@@ -12,15 +12,35 @@
         $pdo = new PDO('mysql:host='.$host.';dbname='.$db_name, $db_user, $db_password );
     } catch (Exception $e) {
     }
-
-
-renderTemplate('homepage');
-
-
-function renderTemplate($templateName) {
-	$path = __DIR__ . '/templates/' . $templateName . '.php';
-	require_once $path;
-}
-
-
 ?>
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+<title>Moja pierwsza gra przeglądarkowa</title>
+<meta charset="utf-8"/>
+<meta name="description" content="Pierwsza gra posiadająca własny system logowania i surowców.Wciągająca jak żadna inna!!"/>
+<meta name="keywords" content="gra,strategia,logowanie,najlepsza"/>
+<meta http-equiv="X-UA Compatible" content="IE=edge,chrome=1"/>
+<link rel="stylesheet" href="style.css" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
+</head>
+<body>
+<div id="container">
+	<div id="logo">
+	TIBIJA-Wersja Exclusive
+	</div>
+	<div class="logrej">
+		<form id="login" action="" method="post" >
+			Login:<input type="text" name="user" placeholder="Podaj login" autofocus/></br>
+			Hasło:<input type="password" name="haslo" placeholder="Podaj hasło"/></br>
+			<input type="submit" value="Zaloguj"/>
+		</form>
+	</div>
+	<div class="logrej">
+		<a href="rejestracja.php" id="stworz">Nie masz jeszcze konta?!</br></br>
+		Koniecznie utwórz je w naszym serwisie!!</a>
+	</div>
+	<div style="clear:both"></div>
+</div>
+</body>
+</html>
